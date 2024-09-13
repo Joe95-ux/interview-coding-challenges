2e5d23d0b8af38039510ab792f4ebb6e1235ec8d
joe95-ux <ogorktabi@gmail.com>
2024-09-13 23:22:32 +0200

    solve max subarray

---
+++ maxSubarray.js
@@ -0,0 +1,22 @@
+// Maximum Subarray
+
+// Problem Description:
+// Given an integer array nums, find the contiguous subarray 
+// (containing at least one number) which has the largest sum and return its sum.
+
+// Solution:
+
+function maxSubArray(nums) {
+  let maxSoFar = nums[0];
+  let maxEndingHere = nums[0];
+  
+  for (let i = 1; i < nums.length; i++) {
+    maxEndingHere = Math.max(nums[i], maxEndingHere + nums[i]);
+    maxSoFar = Math.max(maxSoFar, maxEndingHere);
+  }
+  
+  return maxSoFar;
+}
+
+// Test case
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // Output: 6
